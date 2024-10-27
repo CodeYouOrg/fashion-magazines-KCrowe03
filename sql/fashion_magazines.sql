bc000bf8762c1e4d9107c056819892e56295a8ee
KCrowe03 <165347733+KCrowe03@users.noreply.github.com>
2024-10-27 10:28:46 -0400

    completed assignment

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,16 @@
--- Add your sql here
+-- Kristen Crowe 
+-- Write a SQL query that returns the customer name and 
+--total amount due for the customers that have unpaid Fashion Magazine 
+--subscriptions.
+
+SELECT customers.customer_name AS Customer, 
+       PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS "Amount Due"
+FROM orders
+JOIN customers ON orders.customer_id = customers.customer_id
+JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE orders.order_status = 'unpaid'
+  AND subscriptions.description = 'Fashion Magazine'
+GROUP BY customers.customer_name;
+
+
+
